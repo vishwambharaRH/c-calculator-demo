60ab514691a112b21e3190418dab2bde801a694c
Vishwambhara R Hebbalalu <vishwambharah@gmail.com>
2026-09-17 11:52:37 +0530

    test commit

--- src/calculator.c
+++ src/calculator.c
@@ -4,10 +4,13 @@
 double add(double a, double b) { return a + b; }
 double subtract(double a, double b) { return a - b; }
 double multiply(double a, double b) { return a * b; }
-double divide(double a, double b) {
-if (b == 0.0) {
-printf("Error: Division by zero!\n");
-return 0.0;
-}
-return a / b;
+double divide(double a, double b)
+{
+    if (b == 0.0)
+    {
+        printf("Error: Division by zero!\n");
+        return 0.0;
+    }
+    // comment for testing
+    return a / b;
 }
